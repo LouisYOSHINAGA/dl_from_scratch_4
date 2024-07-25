c08ca5020201c69332b7155687cf88c53cca33a7
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-25 23:09:14 +0900

    Add: policy_eval.py

---
+++ chapter04/policy_eval.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append("..")
+from collections import defaultdict
+from common.gridworld import GridWorld
+
+
+def eval_onestep(pi, V, env, gamma=0.9):
+    for state in env.states():
+        if state == env.goal_state:
+            V[state] = 0
+            continue
+
+        action_probs = pi[state]
+        new_V = 0
+
+        for action, action_prob in action_probs.items():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            new_V += action_prob * (r + gamma * V[next_state])
+        V[state] = new_V
+    return V
+
+def policy_eval(pi, V, env, gamma, threshold=0.01):
+    while True:
+        old_V = V.copy()
+        V = eval_onestep(pi, V, env, gamma)
+
+        delta = 0
+        for state in V.keys():
+            t = abs(V[state] - old_V[state])
+            if delta < t:
+                delta = t
+        if delta < threshold:
+            break
+    return V
+
+
+if __name__ == "__main__":
+    env = GridWorld()
+    V = {}
+    for state in env.states():
+        V[state] = 0
+
+    state = (1, 2)
+    print(V[state])
+
+
+    env = GridWorld()
+    V = defaultdict(lambda: 0)
+    state = (1, 2)
+    print(V[state])
+
+
+    pi = defaultdict(lambda: {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25})
+    state = (0, 1)
+    print(pi[state])
+
+
+    env = GridWorld()
+    gamma = 0.9
+    pi = defaultdict(lambda: {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25})
+    V = defaultdict(lambda: 0)
+    V = policy_eval(pi, V, env, gamma)
+    env.render_v(V, pi)
