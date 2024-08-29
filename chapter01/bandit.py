ef7a21c918ef732d1fb365c2c91defeb033543a5
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-08-30 02:28:03 +0900

    Update: bandit.py

--- chapter01/bandit.py
+++ chapter01/bandit.py
@@ -1,77 +1,83 @@
 import numpy as np
+from typing import Literal
 import matplotlib.pyplot as plt
 
 
 class Bandit:
-    def __init__(self, arms=10):
-        self.rates = np.random.rand(arms)
+    def __init__(self, arms: int =10) -> None:
+        self.rng: np.random.Generator = np.random.default_rng()
+        self.rates: float = self.rng.random(arms)
 
-    def play(self, arm):
-        rate = self.rates[arm]
-        if rate > np.random.rand():
-            return 1
-        else:
-            return 0
+    def play(self, arm: int) -> Literal[0, 1]:
+        return 1 if self.rng.random() < self.rates[arm] else 0
 
 
 class Agent:
-    def __init__(self, epsilon, action_size=10):
-        self.epsilon = epsilon
-        self.Qs = np.zeros(action_size)
-        self.ns = np.zeros(action_size)
+    def __init__(self, epsilon: float, action_size: int =10) -> None:
+        self.rng: np.random.Generator = np.random.default_rng()
+        self.epsilon: float = epsilon
+        self.Qs: np.ndarray = np.zeros(action_size)
+        self.ns: np.ndarray = np.zeros(action_size)
 
-    def update(self, action, reward):
+    def update(self, action: int, reward: float) -> None:
         self.ns[action] += 1
         self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]
 
-    def get_action(self):
-        if np.random.rand() < self.epsilon:
-            return np.random.randint(0, len(self.Qs))
-        return np.argmax(self.Qs)
+    def get_action(self) -> int:
+        return self.rng.integers(0, len(self.Qs)) if self.rng.random() < self.epsilon \
+               else np.argmax(self.Qs)
 
 
 if __name__ == "__main__":
+    """
+    # rewards (0-th machine)
     bandit = Bandit()
     for i in range(3):
         print(bandit.play(0))
-
-
-    bandi = Bandit()
-    Q = 0
-    for n in range(1, 11):
-        reward = bandit.play(0)
-        Q += (reward - Q) / n
-        print(Q)
-
-
+    """
+    """
+    # average of rewards (0-th machine)
     bandit = Bandit()
-    Qs = np.zeros(10)
-    ns = np.zeros(10)
-    for n in range(10):
-        action = np.random.randint(0, 10)
-        reward = bandit.play(action)
+    Q: float = 0
+    for n in range(1, 11):
+        reward: Literal[0, 1] = bandit.play(0)
+        Q += (reward - Q) / n
+        print(f"estimated probability: {Q}")
+    print(f"correct answer: {bandit.rates[0]}")
+    """
+    """
+    # average of rewards
+    arms: int = 10
+    bandit = Bandit(arms)
+    rng: np.random.Generator = np.random.default_rng()
+    ns: np.ndarray = np.zeros(arms)
+    Qs: np.ndarray = np.zeros(arms)
+    for n in range(100):
+        action: int = rng.integers(arms)
+        reward: Literal[0, 1] = bandit.play(action)
         ns[action] += 1
         Qs[action] += (reward - Qs[action]) / ns[action]
-        print(Qs)
+        print(f"estimated probabilities: {Qs}")
+    print(f"correct answers: {bandit.rates}")
+    """
 
-
-    steps = 1000
-    epsilon = 0.1
+    steps: int = 1000
 
     bandit = Bandit()
-    agent = Agent(epsilon)
-    total_reward = 0
-    total_rewards = []
-    rates = []
+    agent = Agent(epsilon=0.1)
+
+    total_reward: int = 0
+    total_rewards: list[int] = []
+    rates: float = []
 
     for step in range(steps):
-        action = agent.get_action()
-        reward = bandit.play(action)
+        action: int = agent.get_action()
+        reward: Literal[0, 1] = bandit.play(action)
         agent.update(action, reward)
         total_reward += reward
         total_rewards.append(total_reward)
         rates.append(total_reward / (step+1))
-    print(total_reward)
+    print(f"{total_reward=}")
 
     plt.xlabel("steps")
     plt.ylabel("total reward")
