b55bbd2960cc903db84d530b2af96cb9f9284727
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-04 22:39:39 +0900

    Update: dp_inplace.py

--- chapter04/dp_inplace.py
+++ chapter04/dp_inplace.py
@@ -1,17 +1,45 @@
-V = {'L1': 0.0, 'L2': 0.0}
+from typing import TypeAlias, Literal
 
-cnt = 0
-while True:
-    t = 0.5 * (-1 + 0.9 * V['L1']) + 0.5 * (1 + 0.9 * V['L2'])
-    delta = abs(t - V['L1'])
-    V['L1'] = t
+State: TypeAlias = Literal["L1", "L2"]
+Action: TypeAlias = Literal["left", "right"]
 
-    t = 0.5 * (0 + 0.9 * V['L1']) + 0.5 * (-1 + 0.9 * V['L2'])
-    delta = max(delta, abs(t - V['L2']))
-    V['L2'] = t
 
-    cnt += 1
-    if delta < 0.0001:
-        print(V)
-        print(cnt)
-        break
+states: list[State] = ["L1", "L2"]  # states: s
+actions: list[Action] = ["left", "right"]  # actions: a
+
+pi: dict[tuple[State, Action], float] = {
+    # state transition probability: p(s'|s,a)
+    ("L1", "left"): 0.5, ("L1", "right"): 0.5,
+    ("L2", "left"): 0.5, ("L2", "right"): 0.5,
+}
+# state transition is deterministic: p(s'|s,a) -> s' = f(s,a)
+r: dict[tuple[State, Action, State], float] = {
+    # reward function: r(s,a,s')
+    ("L1", "left", "L1"): -1, ("L1", "right", "L2"): +1,
+    ("L2", "left", "L1"):  0, ("L2", "right", "L2"): -1,
+}
+gamma: float = 0.9  # discount rate: gamma
+
+V: dict[State, float] = {"L1": 0.0, "L2": 0.0}  # state value function: V_{k}(s)
+
+
+if __name__ == "__main__":
+    cnt: int = 0
+    while True:
+        deltas: dict[State, float] = {state: 0 for state in states}
+
+        for state in states:
+            temp: float = 0
+            for action, next_state in zip(actions, states):
+                # V_{k+1}(s) = \sum_{a} pi(a|s) ( r(s,a,s') + gamma * V_{k}(s') )
+                temp += pi[(state, action)] * (r[(state, action, next_state)] + gamma * V[next_state])
+
+            deltas[state] = abs(temp - V[state])
+            V[state] = temp
+        cnt += 1
+
+        if max(deltas.values()) < 0.0001:
+            break
+
+    print(f"{V=}")
+    print(f"{cnt}")
