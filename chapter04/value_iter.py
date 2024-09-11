6ff9ef167a50c55fd954f3a5de3dc5572ce5a1c4
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-11 22:19:31 +0900

    Update: value_iter.py

--- chapter04/value_iter.py
+++ chapter04/value_iter.py
@@ -1,48 +1,49 @@
-import sys
-sys.path.append("..")
+if "__file__" in globals():
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from collections import defaultdict
 from common.gridworld import GridWorld
-from chapter04.policy_iter import greedy_policy
+from policy_iter import greedy_policy
+
+from typing import TypeAlias, Literal
+State: TypeAlias = tuple[int, int]
+Action: TypeAlias = Literal[0, 1, 2, 3]
+StateValueFunction: TypeAlias = tuple[State, float]  # v(s)
+DiscountRate: TypeAlias = float  # gamma
+Policy: TypeAlias = dict[State, tuple[Action, float]]  # pi(a|s)
 
 
-def value_iter_onestep(V, env, gamma):
+def value_iter_onestep(V: StateValueFunction, env: GridWorld, gamma: DiscountRate) -> StateValueFunction:
     for state in env.states():
         if state == env.goal_state:
             V[state] = 0
             continue
 
-        action_values = []
+        action_values: dict[Action, float] = {}  # {a: r(s,a,s') + gamma * v(s'))
         for action in env.actions():
-            next_state = env.next_state(state, action)
-            r = env.reward(state, action, next_state)
-            value = r + gamma * V[next_state]
-            action_values.append(value)
-        V[state] = max(action_values)
+            next_state: State = env.next_state(state, action)  # s'
+            action_values[action] = env.reward(state, action, next_state) + gamma * V[next_state]  # r(s,a,s') + gamma * v(s')
+        V[state] = max(action_values.values())
     return V
 
-def value_iter(V, env, gamma, threshold=0.001, is_render=True):
+def value_iter(V: StateValueFunction, env: GridWorld, gamma: DiscountRate,
+               threshold: float =0.001, is_render: bool =True) -> StateValueFunction:
     while True:
         if is_render:
             env.render_v(V)
 
-        old_V = V.copy()
-        V = value_iter_onestep(V, env, gamma)
+        old_V: StateValueFunction = V.copy()  # V_{k}
+        V = value_iter_onestep(V, env, gamma)  # V_{k+1}
 
-        delta = 0
-        for state in V.keys():
-            t = abs(V[state] - old_V[state])
-            if delta < t:
-                delta = t
-        if delta < threshold:
+        if max([V[state] - old_V[state] for state in V.keys()]) < threshold:
             break
     return V
 
 
 if __name__ == "__main__":
-    V = defaultdict(lambda: 0)
     env = GridWorld()
-    gamma = 0.9
-
+    gamma: DiscountRate = 0.9
+    V: StateValueFunction = defaultdict(lambda: 0)
     V = value_iter(V, env, gamma)
-    pi = greedy_policy(V, env, gamma)
+    pi: Policy = greedy_policy(V, env, gamma)
     env.render_v(V, pi)
