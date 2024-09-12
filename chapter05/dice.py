17e676e781e3c05df2b03f3b33c93d0e0e8f8e83
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-12 21:19:57 +0900

    Update: dice.py

--- chapter05/dice.py
+++ chapter05/dice.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 
-def sample(dices=2):
-    x = 0
+def sample(dices: int =2) -> int:
+    rng: np.random.Generator = np.random.default_rng()
+    x: int = 0
     for _ in range(dices):
-        x += np.random.choice([1, 2, 3, 4, 5, 6])
+        x += rng.choice([1, 2, 3, 4, 5, 6])
     return x
 
 
@@ -13,10 +14,11 @@ if __name__ == "__main__":
     print(sample())
     print(sample())
 
-    trial = 1000
-    V, n = 0, 0
+    trial: int = 1000
+    V: float = 0
+    n: int = 0
     for _ in range(trial):
-        s = sample()
+        s: int = sample()
         n += 1
         V += (s - V) / n
-        print(V)
+    print(V)
