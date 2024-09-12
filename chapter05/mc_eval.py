605935000b5c0b1c8efbf1c80112d16e11fb123c
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-12 22:11:23 +0900

    Update: mc_eval.py

--- chapter05/mc_eval.py
+++ chapter05/mc_eval.py
@@ -1,54 +1,61 @@
-import sys
-sys.path.append("..")
+if "__file__" in globals():
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import numpy as np
 from collections import defaultdict
 from common.gridworld import GridWorld
 
+from typing import TypeAlias, Literal
+State: TypeAlias = tuple[int, int]  # s
+Action: TypeAlias = Literal[0, 1, 2, 3]  # a
+Policy: TypeAlias = dict[State, tuple[Action, float]]  # pi(a|s)
+Reward: TypeAlias = float  # r
+DiscountRate: TypeAlias = float  # gamma
+StateValueFunction: TypeAlias = dict[State, float]  # v(s)
+
 
 class RandomAgent:
     def __init__(self):
-        self.gamma = 0.9
-        self.action_size = 4
+        self.rng: np.random.Generator = np.random.default_rng()
+        self.gamma: DiscountRate = 0.9  # gamma
+        self.actions: list[Action] = [0, 1, 2, 3]  # A
+        self.action_size: int = len(self.actions)  # |A|
 
-        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
-        self.pi = defaultdict(lambda: random_actions)
-        self.V = defaultdict(lambda: 0)
-        self.cnts = defaultdict(lambda: 0)
-        self.memory = []
+        self.pi: Policy = defaultdict(lambda: {action: 1/self.action_size for action in self.actions})
+        self.V: StateValueFunction = defaultdict(lambda: 0)
 
-    def get_action(self, state):
-        action_probs = self.pi[state]
-        actions = list(action_probs.keys())
-        probs = list(action_probs.values())
-        return np.random.choice(actions, p=probs)
+        self.cnts: dict[State, int] = defaultdict(lambda: 1)
+        self.memory: list[tuple[State, Action, Reward]] = []
 
-    def add(self, state, action, reward):
-        data = (state, action, reward)
-        self.memory.append(data)
+    def get_action(self, state: State) -> Action:
+        action_probs: dict[Action, float] = self.pi[state]
+        return self.rng.choice(list(action_probs.keys()), p=list(action_probs.values()))
 
-    def reset(self):
-        self.memory.clear()
+    def add(self, state: State, action: Action, reward: Reward) -> None:
+        self.memory.append((state, action, reward))
 
-    def eval(self):
-        G = 0
-        for data in reversed(self.memory):
-            state, action, reward = data
-            G = self.gamma * G + reward
+    def eval(self) -> None:
+        G: float = 0
+        for state, _, reward in reversed(self.memory):
+            G = reward + self.gamma * G  # G_{s} = R_{t} + gamma * R_{t+1} + ... = R_{t} + gamma * G_{s'}
+            self.V[state] += (G - self.V[state]) / self.cnts[state]  # V_{n} = V_{n-1} + (s_{n} - V_{n-1}) / n
             self.cnts[state] += 1
-            self.V[state] += (G - self.V[state]) / self.cnts[state]
+
+    def reset(self) -> None:
+        self.memory.clear()
 
 
 if __name__ == "__main__":
     env = GridWorld()
     agent = RandomAgent()
 
-    episodes = 1000
+    episodes: int = 1000
     for episode in range(episodes):
-        state = env.reset()
+        state: State = env.reset()
         agent.reset()
 
         while True:
-            action = agent.get_action(state)
+            action: Action = agent.get_action(state)
             next_state, reward, done = env.step(action)
             agent.add(state, action, reward)
             if done:
