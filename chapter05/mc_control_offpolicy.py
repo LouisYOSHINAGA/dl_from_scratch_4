583da3bccfe18300c65ff4a2ab032c8c2478cb23
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-18 23:12:53 +0900

    Update: mc_control_offpolicy.py

--- chapter05/mc_control_offpolicy.py
+++ chapter05/mc_control_offpolicy.py
@@ -1,64 +1,67 @@
-import sys
-sys.path.append("..")
+if "__file__" in globals():
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import numpy as np
 from collections import defaultdict
 from common.gridworld import GridWorld
 from common.utils import greedy_probs
 
+from typing import TypeAlias, Literal
+State: TypeAlias = tuple[int, int]  # s
+Action: TypeAlias = Literal[0, 1, 2, 3]  # a
+Policy: TypeAlias = dict[State, dict[Action, float]]  # pi(a|s)
+Reward: TypeAlias = float  # r
+DiscountRate: TypeAlias = float  # gamma
+ActionValueFunction: TypeAlias = dict[Action, float]  # Q(s,a)
+
 
 class McOffPolicyAgent:
-    def __init__(self):
-        self.gamma = 0.9
-        self.epsilon = 0.1
-        self.alpha = 0.2
-        self.action_size = 4
+    def __init__(self) -> None:
+        self.rng: np.random.Generator = np.random.default_rng()
+        self.gamma: DiscountRate = 0.9  # gamma
+        self.actions: list[Action] = [0, 1, 2, 3]  # A
+        self.action_size = len(self.actions)  # |A|
 
-        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
-        self.pi = defaultdict(lambda: random_actions)
-        self.b = defaultdict(lambda: random_actions)
-        self.Q = defaultdict(lambda: 0)
-        self.memory = []
+        self.pi: Policy = defaultdict(lambda: {action: 1/self.action_size for action in self.actions})  # pi(a|s)
+        self.b: Policy = defaultdict(lambda: {action: 1/self.action_size for action in self.actions})  # b(a|s)
+        self.Q: ActionValueFunction  = defaultdict(lambda: 0)  # Q(s,a)
 
-    def get_action(self, state):
-        action_probs = self.b[state]
-        actions = list(action_probs.keys())
-        probs = list(action_probs.values())
-        return np.random.choice(actions, p=probs)
+        self.epsilon: float = 0.1
+        self.alpha: float = 0.2
+        self.memory: list[tuple[State, Action, Reward]] = []
 
-    def add(self, state, action, reward):
-        data = (state, action, reward)
-        self.memory.append(data)
+    def get_action(self, state: State) -> Action:
+        action_probs: dict[Action, float] = self.b[state]
+        return self.rng.choice(list(action_probs.keys()), p=list(action_probs.values()))
 
-    def reset(self):
+    def add(self, state: State, action: Action, reward: Reward) -> None:
+        self.memory.append((state, action, reward))
+
+    def update(self) -> None:
+        G: float = 0
+        rho: float = 1
+        for state, action, reward in reversed(self.memory):
+            G = reward + rho * self.gamma * G
+            self.Q[(state, action)] += self.alpha * (G - self.Q[(state, action)])
+            rho *= self.pi[state][action] / self.b[state][action]  # rho = \prod_{t} pi(A_t|S_t)/b(A_t|S_t)
+            self.pi[state] = greedy_probs(Q=self.Q, state=state, epsilon=0)
+            self.b[state] = greedy_probs(Q=self.Q, state=state, epsilon=self.epsilon)
+
+    def reset(self) -> None:
         self.memory.clear()
 
-    def update(self):
-        G = 0
-        rho = 1
 
-        for data in reversed(self.memory):
-            state, action, reward = data
-            key = (state, action)
-
-            G = self.gamma * rho * G + reward
-            self.Q[key] += self.alpha * (G - self.Q[key])
-            rho *= self.pi[state][action] / self.b[state][action]
-
-            self.pi[state] = greedy_probs(self.Q, state, epsilon=0)
-            self.b[state] = greedy_probs(self.Q, state, self.epsilon)
-
-
-if __name__ == "__main__":
+def main() -> None:
     env = GridWorld()
     agent = McOffPolicyAgent()
 
     episodes = 10000
-    for episode in range(episodes):
+    for _ in range(episodes):
         state = env.reset()
         agent.reset()
 
         while True:
-            action = agent.get_action(state)
+            action: Action = agent.get_action(state)
             next_state, reward, done = env.step(action)
             agent.add(state, action, reward)
             if done:
@@ -67,3 +70,7 @@ if __name__ == "__main__":
             state = next_state
 
     env.render_q(agent.Q)
+
+
+if __name__ == "__main__":
+    main()
