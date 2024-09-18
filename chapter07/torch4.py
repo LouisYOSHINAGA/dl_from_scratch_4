2f51cf53b5b9bb5022fc83edda5efcf8033f521d
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-19 01:06:46 +0900

    Update: torch4.py

--- chapter07/torch4.py
+++ chapter07/torch4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch as t
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from typing import Any
 
 
@@ -31,21 +32,45 @@ def update(lr: float, *args: tuple[Any]) -> None:
             arg -= lr * arg.grad
             arg.grad.zero_()
 
+
+def inference(W1: t.Tensor, b1: t.Tensor, W2: t.Tensor, b2: t.Tensor,
+              xrange: tuple[float, float] =(0, 1, 0.01)) -> tuple[t.Tensor, t.Tensor]:
+    pred_x: t.Tensor = t.arange(*xrange).view(-1, 1)
+    with t.no_grad():
+        pred_y: t.Tensor = predict(pred_x, W1, b1, W2, b2)
+    return pred_x, pred_y
+
+def plot_data(data_x: t.Tensor, data_y: t.Tensor,
+              pred_x: t.Tensor|None =None, pred_y: t.Tensor|None =None) -> None:
+    plt.figure(figsize=(8, 6))
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.scatter(data_x, data_y)
+    if pred_x is not None and pred_y is not None:
+        plt.plot(pred_x, pred_y, color=plt.get_cmap("tab10")(1))
+    plt.tight_layout()
+    plt.show()
+
+
 def main() -> None:
     lr: float = 0.2
     iters: int = 10000
 
     x, y = gen_data()
+    plot_data(x, y)  # fig. 7-10
     W1, b1, W2, b2 = init_weight(input=1, hidden=10, output=1)
 
     for i in range(iters):
-        y_pred: t.Tensor = predict(x, W1, b1, W2, b2)
-        loss: t.Tensor = F.mse_loss(y_pred, y)
+        pred_y: t.Tensor = predict(x, W1, b1, W2, b2)
+        loss: t.Tensor = F.mse_loss(pred_y, y)
         loss.backward()
         update(lr, W1, b1, W2, b2)
 
         if (i + 1) % 1000 == 0:
             print(f"[iter{i:04d}] loss = {loss.item():.5f}")
+
+    pred_x, pred_y = inference(W1, b1, W2, b2)
+    plot_data(x, y, pred_x, pred_y)  # fig. 7-12
 
 
 if __name__ == "__main__":
