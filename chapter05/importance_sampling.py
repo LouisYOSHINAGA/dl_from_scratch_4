090640145f50e4c0a8e62153d82601950db5dc13
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-09-18 21:19:16 +0900

    Update: importance_sampling.py

--- chapter05/importance_sampling.py
+++ chapter05/importance_sampling.py
@@ -1,50 +1,48 @@
 import numpy as np
 
-if __name__ == "__main__":
-    x = np.array([1, 2, 3])
-    pi = np.array([0.1, 0.1, 0.8])
+from typing import TypeAlias, Literal
+SampleData: TypeAlias = Literal[1, 2, 3]
+ProbDist: TypeAlias = dict[SampleData, float]
 
-    e = np.sum(x * pi)
-    print(f"E_pi[x] = {e}")
-
-    n = 100
-    samples = []
-    for _ in range(n):
-        s = np.random.choice(x, p=pi)
-        samples.append(s)
-
-    mean = np.mean(samples)
-    var = np.var(samples)
-    print(f"MC: {mean:.2f}, (var: {var:.2f})")
+rng: np.random.Generator = np.random.default_rng()
 
 
-    b = np.array([1/3, 1/3, 1/3])
-    n = 100
-    samples = []
-
-    for _ in range(n):
-        idx = np.arange(len(b))
-        i = np.random.choice(idx, p=b)
-        s = x[i]
-        rho = pi[i] / b[i]
-        samples.append(rho * s)
-
-    mean = np.mean(samples)
-    var = np.var(samples)
-    print(f"IS: {mean:.2f} (var: {var:.2f})")
+xs: list[SampleData] = [1, 2, 3]
+pi: ProbDist = {1: 0.1, 2: 0.1, 3: 0.8}
+b: ProbDist = {1: 1/3, 2: 1/3, 3: 1/3}
+p: ProbDist = {1: 0.2, 2: 0.2, 3: 0.6}
 
 
-    b = np.array([0.2, 0.2, 0.6])
-    n = 100
-    samples = []
+# true value
+e: float = sum([x * prob for x, prob in pi.items()])
+print(f"[True Value] E_pi[x] = {e:.3f}")
 
-    for _ in range(n):
-        idx = np.arange(len(b))
-        i = np.random.choice(idx, p=b)
-        s = x[i]
-        rho = pi[i] / b[i]
-        samples.append(rho * s)
 
-    mean = np.mean(samples)
-    var = np.var(samples)
-    print(f"IS: {mean:.2f} (var: {var:.2f})")
+# estimated value: monte carlo
+n: int = 100
+samples: list[SampleData] = [rng.choice(xs, p=list(pi.values())) for _ in range(n)]
+mean: float = np.mean(samples)
+var: float = np.var(samples)
+print(f"[Estimated Value (MC)] E_pi[x] ~ {mean:.3f} (var: {var:.3f})")
+
+
+# estimated value: importance sampling
+n = 100
+samples = []
+for _ in range(n):
+    x: SampleData = rng.choice(list(b.keys()), p=list(b.values()))
+    samples.append(pi[x]/b[x] * x)  # rho(x) * x = pi(x)/b(x) * x
+mean = np.mean(samples)
+var = np.var(samples)
+print(f"[Estimated Value (IS)] E_pi[x] ~ {mean:.3f} (var: {var:.3f})")
+
+
+# estimated value: importance sampling (low variance)
+n = 100
+samples = []
+for _ in range(n):
+    x: SampleData = rng.choice(list(p.keys()), p=list(p.values()))
+    samples.append(pi[x]/p[x] * x)  # rho(x) * x = pi(x)/b(x) * x
+mean = np.mean(samples)
+var = np.var(samples)
+print(f"[Estimated Value (IS)] E_pi[x] ~ {mean:.3f} (var: {var:.3f})")
