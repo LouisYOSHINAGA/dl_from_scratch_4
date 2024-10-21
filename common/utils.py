3a6bae211c360acc001d22e3f1dcb01e60d2905f
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-10-21 23:46:42 +0900

    Update: utils.py

--- common/utils.py
+++ common/utils.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+from typing import TypeAlias, Literal
+State: TypeAlias = tuple[int, int]
+Action: TypeAlias = Literal[0, 1, 2, 3]
+ActionValue: TypeAlias = float
+ActionValueFunction: TypeAlias = dict[tuple[State, Action], ActionValue]
 
-def greedy_probs(Q, state, epsilon=0, action_size=4):
-    qs = [Q[(state, action)] for action in range(action_size)]
-    max_action = np.argmax(qs)
-    base_prob = epsilon / action_size
-    action_probs = {action: base_prob for action in range(action_size)}
-    action_probs[max_action] += (1 - epsilon)
+
+def greedy_probs(Q: ActionValueFunction, state: State,
+                 epsilon: float =0, actions: list[Action] =[0, 1, 2, 3]) -> dict[Action, float]:
+    max_action: Action = np.argmax([Q[state, action] for action in actions])
+    action_probs: dict[Action, float] = {action: epsilon/len(actions) for action in actions}
+    action_probs[max_action] += 1 - epsilon
     return action_probs
